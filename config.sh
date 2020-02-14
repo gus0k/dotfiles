3a75fbc6e0092f1c443314230ddfbd55cb0aaa30
gus0k <gusok@protonmail.com>
2020-02-14 09:02:42 +0100

    ...

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 git remote set-url origin git@github.com:gus0k/dotfiles.git
 
 sudo ln -sf $DOTFILES/utils/latex_docker.sh /usr/local/bin/latexmk
-sudo ln -sf $DOTFILES/.config $HOME/.config
+sudo ln -sf $DOTFILES/i3 $HOME/.i3
